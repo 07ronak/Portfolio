2dc1b2d38615e949e0d4a8e5c443c0a22baa4bd5
07ronak <ronakhingonia123456789@gmail.com>
2024-12-15 15:51:04 +0530

    Fixed responsivness for welcome message

--- src/components/WelcomeMessage.jsx
+++ src/components/WelcomeMessage.jsx
@@ -22,10 +22,14 @@ const WelcomeMessage = () => {
         className="absolute inset-0 bg-[hsla(197,87%,94%,1)]
 "
       ></div>
-      <div className="inline-block">
-        <span className="relative z-10 inline-block leading-normal font-light animate-typing text-3xl text-black opacity-70 italic overflow-hidden whitespace-nowrap ease-in-out">
+      <div className="inline-block px-4">
+        <span className="relative z-10 inline-block leading-normal font-light animate-typing md:text-3xl text-xl text-black opacity-70 italic overflow-hidden whitespace-nowrap ease-in-out">
           159 million million million, was it?
         </span>
+        {/* <span className="relative z-10 inline-block animate-typing leading-normal font-light text-xl md:text-3xl text-black opacity-70 italic overflow-hidden whitespace-nowrap ease-in-out">
+          <span className="inline-block ">159 million million million,</span>{" "}
+          <span className="md:inline block animate-typingsecond">was it?</span>
+        </span> */}
       </div>
     </div>
   );
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -14,9 +14,14 @@ export default {
           "0%": { width: "0" },
           "100%": { width: "100%" },
         },
+        /* typingsecond: {
+          "0%": { width: "0" },
+          "100%": { width: "100%" },
+        }, */
       },
       animation: {
-        typing: "typing 4s",
+        typing: "typing 2s",
+        /* typingsecond: "typing 2s forwards 2s" // Delays by 2 seconds , */
       },
     },
   },
