b2cb16cb862ebc49602ae5772e26fe6c8320dcba
07ronak <ronakhingonia123456789@gmail.com>
2025-01-09 23:41:43 +0530

    added tweets

--- src/components/Tweets.jsx
+++ src/components/Tweets.jsx
@@ -4,6 +4,8 @@ import { Tweet } from "react-tweet";
 const Tweets = () => {
   return (
     <div className="dark justify-center flex flex-col items-center">
+      <Tweet id="1877417395825901959" />
+      <Tweet id="1877416630004908269" />
       <Tweet id="1876684292878983504" />
       <Tweet id="1876683757908062315" />
       <Tweet id="1876159145554112816" />
