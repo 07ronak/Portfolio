83c4c059f0c1c4ee8477e2501370e26aff019746
07ronak <ronakhingonia123456789@gmail.com>
2025-02-15 13:41:27 +0530

    added the missed tweet in btwn

--- src/components/Tweets.jsx
+++ src/components/Tweets.jsx
@@ -6,6 +6,7 @@ const Tweets = () => {
     <div className="dark justify-center flex flex-col items-center">
       <Tweet id="1890674240921170347" />
       <Tweet id="1890672761837691057" />
+      <Tweet id="1888884390526173537" />
       <Tweet id="1888536532132454876" />
       <Tweet id="1888179897665048766" />
       <Tweet id="1887795637493907569" />
