7870e28e5c53e8d1536bf2f4128e69b45760c85c
Ronak Hingonia <158851792+07ronak@users.noreply.github.com>
2026-07-11 21:33:01 +0530

    updated leetcode section

--- src/components/Leetcode.jsx
+++ src/components/Leetcode.jsx
@@ -3,11 +3,11 @@ import { ExternalLink, Code, Trophy, CheckCircle, Github } from "lucide-react";
 
 const LeetCodeSection = () => {
   const stats = {
-    easy: { solved: 225, total: 949 },
-    medium: { solved: 337, total: 2067},
-    hard: { solved: 101, total: 942 },
-    acceptanceRate: 74.18,  
-    submissions: 2568,
+    easy: { solved: 241, total: 953 },
+    medium: { solved: 365, total: 2081},
+    hard: { solved: 115, total: 951 },
+    acceptanceRate: 74.17,  
+    submissions: 2904,
   };
 
   // Add calculated totals
